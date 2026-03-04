b41e3cc70944336d423b9771e829b42d641614c2
Larry Kim <larry@lunit.io>
2026-03-04 20:43:04 +0900

    chore: move sqlite db to data folder and update gitignore

--- server/src/db/database.ts
+++ server/src/db/database.ts
@@ -1,7 +1,7 @@
 import sqlite3 from "sqlite3";
 import path from "path";
 
-const dbPath = path.resolve(__dirname, "../../database.sqlite");
+const dbPath = path.resolve(__dirname, "../../data/database.sqlite");
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error("Error opening database", err.message);
