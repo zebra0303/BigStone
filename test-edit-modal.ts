d0307d26dfb7dff7c9f579d42b988cadc294fe2b
Larry Kim <larry@lunit.io>
2026-03-06 12:51:10 +0900

    chore: clean up temporary test scripts and resolve workspace dependencies

--- test-edit-modal.ts
+++
@@ -1,3 +0,0 @@
-import * as fs from "fs";
-const file = fs.readFileSync("client/src/features/todo/ui/TodoEditModal.tsx", "utf-8");
-console.log(file.split('\n').filter((l, i) => i > 25 && i < 45).join('\n'));
